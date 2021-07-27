c251a084087b6a09d0f7901c73401d38f2bb01b3
Prasadbabu436 <prasadbabutirumala@gmail.com>
2021-07-27 13:30:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xhz57p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xhz57p)
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+
+<div id="d1">
+<h1> Hello world</h1>
+</div>
+
+
+class myname extends React.Component{
+
+constructor(){
+
+  super();
+
+  this.state={
+    student:["Prasad", "babu", "santosh"]
+  }
+
+}
+ render(){
+
+  return()
+
+
+ }
+
+
+}
+
+ReactDOM.render{<myname/>, document.getElementById(d1)}
